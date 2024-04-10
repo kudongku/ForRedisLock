2e90109ae966b84df3e539ec349046b5fb3f3093
kudongku <kudongku@naver.com>
2024-04-10 23:25:27 +0900

    :construction_worker: test(coupon) : 분산락 미적용 테스트

--- src/test/java/com/example/forredislock/coupon/CouponDecreaseLockTest.java
+++ src/test/java/com/example/forredislock/coupon/CouponDecreaseLockTest.java
@@ -11,7 +11,6 @@ import java.util.concurrent.Executors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -62,5 +61,31 @@ public class CouponDecreaseLockTest {
 
         assertThat(persistCoupon.getAvailableStock()).isZero();
         System.out.println("잔여 쿠폰 개수 = " + persistCoupon.getAvailableStock());
+    }
+
+    @Test
+    void 쿠폰차감_분산락_미적용_동시성100명_테스트() throws InterruptedException {
+        int numberOfThreads = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            executorService.submit(() -> {
+                try {
+                    // 분산락 미적용 메서드 호출
+                    couponDecreaseService.couponDecrease(coupon.getId());
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Coupon persistCoupon = couponRepository.findById(coupon.getId())
+            .orElseThrow(IllegalArgumentException::new);
+
+        assertThat(persistCoupon.getAvailableStock()).isZero();
+        System.out.println("잔여 쿠폰 갯수 = " + persistCoupon.getAvailableStock());
     }
 }
