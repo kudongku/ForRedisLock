20770af8d0948891d3a49368f988469eec9111ec
kudongku <kudongku@naver.com>
2024-04-10 22:43:19 +0900

    :tada: feat(git) : git init commit

---
+++ src/main/java/com/example/forredislock/ForRedisLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.forredislock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForRedisLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForRedisLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forredislock/ForRedisLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forredislock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForRedisLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
